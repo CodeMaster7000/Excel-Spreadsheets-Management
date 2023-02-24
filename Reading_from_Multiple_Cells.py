2612ee2823ae67b1f1e7420fb95194e9085f2505
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2023-02-24 19:25:20 +0000

    Update Reading_from_Multiple_Cells.py

--- Reading_from_Multiple_Cells.py
+++ Reading_from_Multiple_Cells.py
@@ -1,1 +1,16 @@
-
+import openpyxl
+path = "abc.xlsx"
+wb_obj = openpyxl.load_workbook(path)
+sheet_obj = wb_obj.active
+row = sheet_obj.max_row
+column = sheet_obj.max_column
+print("No. of rows:", row)
+print("No. of columns:", column)
+print("\nValues of first column:")
+for i in range(1, row + 1):
+	cell_obj = sheet_obj.cell(row = i, column = 1)
+	print(cell_obj.value)
+print("\nValues of first row:")
+for i in range(1, column + 1):
+	cell_obj = sheet_obj.cell(row = 2, column = i)
+	print(cell_obj.value, end = " ")
